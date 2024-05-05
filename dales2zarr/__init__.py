4528592ea09e8c5b77bdd9e2ffb752994b0c11d4
goord <G.vandenOord@gmail.com>
2024-05-05 15:33:37 +0200

    first commit

---
+++ dales2zarr/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about dales2zarr."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Gijs van den Oord"
+__email__ = "g.vandenoord@esciencecenter.nl"
+__version__ = "0.1.0"
